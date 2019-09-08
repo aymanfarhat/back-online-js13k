eca94b8c1de1fd6076c2dd2eb83a407b1dba1dfc
Ayman <ayman.farhat1@gmail.com>
2019-09-08 17:03:17 +0400

    Added basic sprite movement + webpack development build

--- src/js/game.js
+++ src/js/game.js
@@ -1,3 +1,5 @@
+import Draw from "./draw.js";
+
 class Game {
     constructor(width, height) {
         let container = document.getElementById('game');
@@ -6,12 +8,29 @@ class Game {
         this.currentWidth = width;
         this.currentHeight = height;
         this.context = container.getContext('2d');
+
+        this.frameIndex = 0;
+        this.tickCount = 0;
+        this.ticksPerFrame = 10;
+        this.nFrames = 2;
     }
 
     init() {}
 
     /** Updates the state of all entities in the world */
-    update() {}
+    update() {
+        this.tickCount += 1;
+
+        if (this.tickCount > this.ticksPerFrame) {
+            this.tickCount = 0;
+
+            if(this.frameIndex < this.nFrames - 1) {
+                this.frameIndex += 1;
+            } else {
+                this.frameIndex = 0;
+            }
+        }
+    }
 
     resize() {
         var height = window.height;
@@ -23,29 +42,45 @@ class Game {
     }
     /** Loops through all the entities in the world and renders them */
     render() {
-        this.context.beginPath();
-        this.context.rect(100, 100, 20, 20);
-        this.context.fillStyle = "#ddbeac";
-        this.context.fill();
+        this.context.clearRect(0, this.currentHeight - 100, 86, 94);
 
         var trex = document.getElementById('trex');
         var f = trex.height / trex.width;
         var newHeight = this.currentWidth * f;
-        this.context.drawImage(trex, 0, (this.currentHeight-trex.height - 10), trex.width, trex.height);
+        this.context.drawImage(
+            trex, 
+            (this.frameIndex * 86),
+            0,
+            86,
+            94,
+            0,
+            this.currentHeight - 100,
+            86,
+            94
+            );
+    }
+
+    loop() {
+        //window.requestAnimationFrame(this.loop());
+
+        this.update();
+        this.render();
     }
 }
 
 let game = new Game(400, 600);
 
+function loop() {
+    window.requestAnimationFrame(loop);
+    game.update();
+    game.render();
+}
+
 window.addEventListener('load', function() {
     game.resize();
+    loop();
 });
 
 window.addEventListener('resize', function() {
     game.resize();
-});
-
-window.requestAnimationFrame(function() {
-    game.update();
-    game.render();
 });
